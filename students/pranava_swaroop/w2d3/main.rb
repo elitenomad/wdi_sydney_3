ab1146dfe9557f6624c0f9fdd6dba60900f76ec1
elitenomad <stalin.pranava@gmail.com>
2014-01-17 10:33:31 +1100

    W2D2 Home work, W2D3 Home work, W2D4 still pending

---
+++ students/pranava_swaroop/w2d3/main.rb
@@ -0,0 +1,62 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'pony'
+
+helpers do
+  	def configure_pony
+	  Pony.options = {
+	    :via => :sendmail,
+	    :via_options => { 
+	      :address              => 'smtp.gmail.com', 
+	      :port                 => '587',  
+	      :user_name            => ENV['SENDGRID_USERNAME'], 
+	      :password             => ENV['SENDGRID_PASSWORD'], 
+	      :authentication       => :plain, 
+	      :enable_starttls_auto => true,
+	      :domain               => 'gmail.com'
+	    }    
+	  }
+	end
+
+end
+
+get '/' do 
+	erb :index
+end
+
+
+get '/home' do 
+	erb :index
+end
+
+get '/about' do
+	erb :about
+end
+
+
+
+post '/contact' do 
+  configure_pony
+  name = params[:contact_name]
+  sender_email = params[:contact_email]
+  message = params[:contact_message]
+  logger.error params.inspect
+
+
+  begin
+    Pony.mail(:from => "stalin.pranava@gmail.com",
+      :to => 'stalin.pranava@gmail.com', 
+      :subject =>"#{name} has contacted you",
+      :body => "#{message}",
+      :via => :sendmail) 
+
+    redirect '/home'
+  rescue
+    @exception = $!
+    erb :index
+  end
+end
+
+not_found do
+	erb :not_found
+end
