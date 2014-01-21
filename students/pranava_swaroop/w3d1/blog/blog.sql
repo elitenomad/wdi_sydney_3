a1a382664935a4a7523d21a72e52be540862d52b
elitenomad <stalin.pranava@gmail.com>
2014-01-21 22:57:15 +1100

    W3D2 home work. Add comments section

--- students/pranava_swaroop/w3d1/blog/blog.sql
+++ students/pranava_swaroop/w3d1/blog/blog.sql
@@ -8,3 +8,11 @@ create table blog(
 );
 
 INSERT INTO blog  (title, description, bodydesc, author, created_at) values ('Sinatra Postgres SQL app','Practicing Sinatra APP ice.','Practicing Sinatra APP ice.','Pranava','2014-01-20');
+
+
+create table comments(
+	id serial8 primary key,
+	comment text,
+	author varchar(50),
+	created_at timestamp
+);
--- students/pranava_swaroop/w3d1/blog/main.rb
+++ students/pranava_swaroop/w3d1/blog/main.rb
@@ -4,6 +4,7 @@ require 'pry'
 require 'active_support/all'
 require 'pg'
 
+
 helpers do
 	def run_sql(sql)
 		#connect to cats db
@@ -15,10 +16,25 @@ helpers do
 		# return the result of the sql
 		res
 	end
+
+	def self.escape_characters_in_string(string)
+	    pattern = /(\'|\"|\.|\*|\/|\-|\\)/
+	    string.gsub(pattern){|match|"\\"  + match}
+	end
 end
 
-get '/' do 
-	sql ="SELECT * FROM blog"
+get '/' do
+	n = params[:sort]
+	if n.nil? || n.empty?
+		sql ="SELECT * FROM blog order by created_at desc"
+	else
+		if n=='updated_at'
+			sql = "SELECT * FROM blog order by #{n} desc"
+		else
+			sql = "SELECT * FROM blog order by #{n}"
+		end
+	end
+
 	@blogs = run_sql(sql)
 
 	erb :home
@@ -29,6 +45,7 @@ get '/posts/:id' do
 	res = run_sql(sql)
 	@blog = res[0]
 
+	@comments = run_sql("SELECT * from comments where blog_id=#{params[:id]}")
 	erb :show
 end
 
@@ -54,13 +71,15 @@ post '/posts/:id/edit' do
 	res = run_sql(sql)
 	@blog = res[0]
 
+	
+
 	erb :edit
 end	
 
 put '/posts/:id/update' do
 	now = Time.now
 	sql = "UPDATE BLOG set title='#{params[:title]}',description='#{params[:description]}'
-			,bodydesc='#{params[:bodydesc]}',author='#{params[:author]}',created_at='#{now}' 
+			,bodydesc='#{params[:bodydesc]}',author='#{params[:author]}',updated_at='#{now}' 
 			where id='#{params[:id]}'"
 	res = run_sql(sql)
 	
@@ -68,10 +87,28 @@ put '/posts/:id/update' do
 end	
 
 delete '/posts/:id/destroy' do
+	run_sql("DELETE FROM COMMENTS where blog_id=#{params[:id]}")
 	sql = "DELETE  from blog where id=#{params[:id]}"
 	res = run_sql(sql)
 
 	redirect to '/'
 end
 
+get '/gallery' do
+	sql = "select author,count(*) from blog group by author"
+	@rows = run_sql(sql)
+
+	erb :gallery
+end
+
+
+post '/posts/:id/comment/create' do
+	now = Time.now
+	sql = "INSERT INTO COMMENTS (comment,author,created_at,blog_id) values 
+			('#{params[:comment]}','#{params[:author]}','#{now}','#{params[:id]}')"
+
+	run_sql(sql)
+
+	redirect to "/posts/#{params[:id]}"
+end
 	
